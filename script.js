2ee43f2bf2e13d20f8e51ac5928303942df0288e
Margaret <margaret.r.stern@gmail.com>
2022-07-04 17:04:13 -0400

    TMDB homework complete

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-const API_KEY = ''
+const API_KEY = 'fd19a7c36d0b40e78c8903095a124166'
 const DOMAIN = 'https://api.themoviedb.org/3'
 const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/original'
+const searchButton = document.getElementById('search')
+const searchInput = document.getElementById('search-input')
+let movieList = document.getElementById('img-div')
+
+const renderList = (movies) => {
+  for (let i = 0; i < movies.length; i++) {
+    let movieTitle = document.createElement('li')
+    movieTitle.append(movies[i].original_title)
+    let moviePoster = document.createElement('img')
+    moviePoster.append(movies[i].poster_path)
+    console.log(movieTitle)
+    console.log(moviePoster)
+    document.body.append(movieTitle)
+    document.querySelector('.movie-list').appendChild(moviePoster)
+    moviePoster.setAttribute(
+      'src',
+      `https://api.themoviedb.org/3/search/movie?query=${moviePoster}&api_key=${API_KEY}`
+    )
+  }
+}
+
+searchButton.addEventListener('click', async () => {
+  let movieTitle = searchInput.value
+  let response = await axios.get(
+    `https://api.themoviedb.org/3/search/movie?query=${movieTitle}&api_key=${API_KEY}`
+  )
+  let movieResults = response.data.results
+  renderList(movieResults)
+})
